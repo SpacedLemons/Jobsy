dfcf6ce36c3bf1b69d0b969837d9e34a7cce1b68
SpacedLemons <v2gamingcentral@gmail.com>
2025-01-26 14:11:14 +0000

    Aligning welcome view with Figma design

--- Jobsy/Onboarding/WelcomeView.swift
+++ Jobsy/Onboarding/WelcomeView.swift
@@ -19,19 +19,26 @@ struct WelcomeView: View {
 
             Text("Which of these applies to you?")
                 .font(.title2)
-                .padding(.bottom, 40)
+                .padding(.bottom, 20)
 
             VStack(spacing: 20) {
                 Button(action: { viewModel.selectRole(.recruiter) }, label: {
                     Text("Recruiter")
                 })
+                .accessibilityLabel("Select Recruiter Role")
                 .buttonStyle(.onboardingStyle())
                 Button(action: { viewModel.selectRole(.candidate) }, label: {
                     Text("Candidate")
                 })
                 .buttonStyle(.onboardingStyle())
+                .accessibilityLabel("Select Candidate Role")
             }
             .padding(.horizontal, 60)
+
+            Text("Hint: A candidate is someone applying for jobs")
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .padding(.top, 20)
             Spacer()
         }
         .padding()
